b083857c8a6d1d15cd116b646f7040b8ac585ee8
lenlynch <lenard.lynch@pobox.com>
2016-08-16 22:58:06 -0500

    first try

---
+++ README.md
@@ -0,0 +1,66 @@
+# homebrew-wireshark-f5-ethtrailer
+
+#[![Build
+#Status]
+
+Homebrew formula with additional patches for building and installing F5 plugin to process extension data added to pcapng capture files on F5 network gear.
+
+- [F5 Ethtrailer
+  Plugin](https://devcentral.f5.com/wiki/advdesignconfig.F5WiresharkPlugin.ashx):
+  enable with `--with-f5-ethtrailer`. 
+  
+  Note:  A DevCentral account is necessary to access these URI.  See [[Source] (https://devcentral.f5.com/getting-started)]
+  The above URI is documentation provided by F5 Networks, the source for the plugin URI follows:
+  [[Source](https://devcentral.f5.com/wiki/GetFile.aspx?Page=AdvDesignConfig.F5WiresharkPlugin&File=wireshark2.plugin.f5ethtrailer.1.11.tar.gz)]
+
+## How to install
+
+If you had previously installed the default homebrew mutt, you must uninstall
+that version first:
+
+```
+> brew uninstall wireshark
+```
+
+Then proceed with installation based on custom formula:
+
+```bash
+> brew tap lenlynch/wireshark-f5-ethtrailer
+# There will be a warning regarding overriding existing formula 'wireshark'
+
+> brew options lenlynch/wireshark/wireshark-f5-ethtrailer
+# List of available options
+# Note that not all combinations are always possible (due to potentially
+# conflicting patches).
+
+--with-gtk+
+       	Build the wireshark command with gtk+
+--with-gtk+3
+       	Build the wireshark command with gtk+3
+--with-headers
+       	Install Wireshark library headers for plug-in development
+--with-libsmi
+       	Build with libsmi support
+--with-lua
+       	Build with lua support
+--with-portaudio
+       	Build with portaudio support
+--with-qt5
+       	Build the wireshark command with Qt5 (can be used with or without either GTK option)
+--without-c-ares
+       	Build without c-ares support
+--without-geoip
+       	Build without geoip support
+--HEAD
+       	Install HEAD version
+--with-f5-ethtrailer
+		Build and Install F5 Ethtrailer plugin
+				   
+
+> brew install lenlynch/wireshark/wireshark-f5-ethtrailer --with-gtk+3 \
+  --with-lua --with-headers --with-qt5 \
+  --with-wireshark-f5-ethtrailer
+# Compile and install customized wireshark with F5 plugin support
+# This is an example, refer to the command above to see the
+# available options.
+```
---
+++ wireshark.rb
@@ -0,0 +1,155 @@
+class Wireshark < Formula
+  desc "Graphical network analyzer and capture tool"
+  homepage "https://www.wireshark.org"
+  url "https://www.wireshark.org/download/src/all-versions/wireshark-2.0.5.tar.bz2"
+  mirror "https://1.eu.dl.wireshark.org/src/wireshark-2.0.5.tar.bz2"
+  sha256 "0ce0241330828973f5b4efee422a3760cab8ce0b41e7721c4b9fd185be1bb10b"
+
+  head "https://code.wireshark.org/review/wireshark", :using => :git
+
+  bottle do
+    sha256 "0a9f98451f8ba2d167c9e75692794e4eced5181ae61cf8db349e371ff0061bc2" => :el_capitan
+    sha256 "2f6c421f4f9e8aff005d0cf0dbfc3c68b2114189b85fda10d7461b151ac1fa9d" => :yosemite
+    sha256 "54e3add2e9631aa400bca0f06a9952dc16d620a2db3a21f1bb450e94c3f8f493" => :mavericks
+  end
+
+  option "with-gtk+3", "Build the wireshark command with gtk+3"
+  option "with-gtk+", "Build the wireshark command with gtk+"
+  option "with-qt5", "Build the wireshark command with Qt5 (can be used with or without either GTK option)"
+  option "with-headers", "Install Wireshark library headers for plug-in development"
+  option "with-f5-ethtrailer", "Build and Install F5 Networks plugin"
+
+  deprecated_option "with-qt" => "with-qt5"
+
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "glib"
+  depends_on "gnutls"
+  depends_on "libgcrypt"
+  depends_on "dbus"
+  depends_on "geoip" => :recommended
+  depends_on "c-ares" => :recommended
+  depends_on "libsmi" => :optional
+  depends_on "lua" => :optional
+  depends_on "portaudio" => :optional
+  depends_on "qt5" => :optional
+  depends_on "gtk+3" => :optional
+  depends_on "gtk+" => :optional
+  depends_on "gnome-icon-theme" if build.with? "gtk+3"
+  depends_on "f5-ethtrailer" if build.with? "headers"
+
+  resource "libpcap" do
+    url "http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz"
+    sha256 "7ad3112187e88328b85e46dce7a9b949632af18ee74d97ffc3f2b41fe7f448b0"
+  end
+
+  def install
+    if MacOS.version <= :mavericks
+      resource("libpcap").stage do
+        system "./configure", "--prefix=#{libexec}/vendor",
+                              "--enable-ipv6",
+                              "--disable-universal"
+        system "make", "install"
+      end
+      ENV.prepend_path "PATH", libexec/"vendor/bin"
+      ENV.prepend "CFLAGS", "-I#{libexec}/vendor/include"
+      ENV.prepend "LDFLAGS", "-L#{libexec}/vendor/lib"
+    end
+
+    args = std_cmake_args
+    args << "-DENABLE_GNUTLS=ON" << "-DENABLE_GCRYPT=ON"
+
+    if build.with? "qt5"
+      args << "-DBUILD_wireshark=ON"
+      args << "-DENABLE_APPLICATION_BUNDLE=ON"
+      args << "-DENABLE_QT5=ON"
+    else
+      args << "-DBUILD_wireshark=OFF"
+      args << "-DENABLE_APPLICATION_BUNDLE=OFF"
+    end
+
+    if build.with?("gtk+3") || build.with?("gtk+")
+      args << "-DBUILD_wireshark_gtk=ON"
+      args << "-DENABLE_GTK3=" + ((build.with? "gtk+3") ? "ON" : "OFF")
+      args << "-DENABLE_PORTAUDIO=ON" if build.with? "portaudio"
+    else
+      args << "-DBUILD_wireshark_gtk=OFF"
+      args << "-DENABLE_PORTAUDIO=OFF"
+    end
+
+    if build.with? "geoip"
+      args << "-DENABLE_GEOIP=ON"
+    else
+      args << "-DENABLE_GEOIP=OFF"
+    end
+
+    if build.with? "c-ares"
+      args << "-DENABLE_CARES=ON"
+    else
+      args << "-DENABLE_CARES=OFF"
+    end
+
+    if build.with? "libsmi"
+      args << "-DENABLE_SMI=ON"
+    else
+      args << "-DENABLE_SMI=OFF"
+    end
+
+    if build.with? "lua"
+      args << "-DENABLE_LUA=ON"
+    else
+      args << "-DENABLE_LUA=OFF"
+    end
+
+    system "cmake", *args
+    system "make"
+    ENV.deparallelize # parallel install fails
+    system "make", "install"
+
+    if build.with? "qt5"
+      prefix.install bin/"Wireshark.app"
+      bin.install_symlink prefix/"Wireshark.app/Contents/MacOS/Wireshark"
+    end
+
+    if build.with? "headers"
+      (include/"wireshark").install Dir["*.h"]
+      (include/"wireshark/epan").install Dir["epan/*.h"]
+      (include/"wireshark/epan/crypt").install Dir["epan/crypt/*.h"]
+      (include/"wireshark/epan/dfilter").install Dir["epan/dfilter/*.h"]
+      (include/"wireshark/epan/dissectors").install Dir["epan/dissectors/*.h"]
+      (include/"wireshark/epan/ftypes").install Dir["epan/ftypes/*.h"]
+      (include/"wireshark/epan/wmem").install Dir["epan/wmem/*.h"]
+      (include/"wireshark/wiretap").install Dir["wiretap/*.h"]
+      (include/"wireshark/wsutil").install Dir["wsutil/*.h"]
+    end
+  end
+
+    if build.with? "f5-ethtrailer"
+      patch do
+        # source:  https://devcentral.f5.com/wiki/GetFile.aspx?Page=AdvDesignConfig.F5WiresharkPlugin&File=wireshark2.plugin.f5ethtrailer.1.11.tar.gz
+        url "https://raw.githubusercontent.com/lenlynch/homebrew-wireshark-f5-ethtrailer/master/patch/f5ethtrailer.makefiles.2.0.1.patch"
+        sha256 "6abbc36c7b9b0e88dedd5e84e9931a658e76c629e684c14b2b97f547a37a7eb9"
+      end
+    end
+
+  def caveats; <<-EOS.undent
+    If your list of available capture interfaces is empty
+    (default OS X behavior), try installing ChmodBPF from homebrew cask:
+
+      brew cask install wireshark-chmodbpf
+
+    This creates an 'access_bpf' group and adds a launch daemon that changes the
+    permissions of your BPF devices so that all users in that group have both
+    read and write access to those devices.
+
+    See bug report:
+      https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=3760
+    EOS
+  end
+
+  test do
+    system bin/"randpkt", "-b", "100", "-c", "2", "capture.pcap"
+    output = shell_output("#{bin}/capinfos -Tmc capture.pcap")
+    assert_equal "File name,Number of packets\ncapture.pcap,2\n", output
+  end
+end
